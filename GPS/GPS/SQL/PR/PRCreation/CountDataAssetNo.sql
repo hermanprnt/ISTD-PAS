ee57acf25c5e20a58ea31b422e9a069d3d67aaeb
Ahmad Thabib Mubarok <ahmadthabibmubarok@gmail.com>
2022-10-13 15:25:23 +0700

    update Get Asset No
    update Get Asset No

--- GPS/GPS/SQL/PR/PRCreation/CountDataAssetNo.sql
+++ GPS/GPS/SQL/PR/PRCreation/CountDataAssetNo.sql
@@ -1,24 +1,74 @@
-﻿IF(@Param = '')
+﻿IF(@Param = '') --IF Query called from screen Vendor Master
 BEGIN
-	SELECT COUNT(1)
-	FROM [FAMS_DB].[FAMS_DB].[dbo].[GET_LIST_ASSET]
-		WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+	DECLARE @@WbsParamSystem VARCHAR (10) = (SELECT TOP 1 ISNULL(SYSTEM_VALUE,'') FROM TB_M_SYSTEM WHERE FUNCTION_ID='ASSET' AND SYSTEM_CD='WBS_PARAM')
+	IF @wbsno LIKE CONCAT(@@WbsParamSystem,'%')
+	BEGIN
+	SELECT COUNT(*) FROM (
+			 SELECT ASSET_NO
+			,SUB_ASSET_NO
+			,ASSET_CATEGORY
+			,CLASS_ID
+			,LOCATION_ID
+			,ASSET_DESC
+			,DIVISION_ID_HR
+			,COST_CENTER
+			,CREATED_DATE 
+			,WBS_NO
+			FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY NOT LIKE '%AU%'
 					AND isnull(@DIVISION_PARAM, '') <> ''
 					OR (isnull(@DIVISION_PARAM, '') = '')))
 				/* FID.Ridwan: 20220712*/
-				AND (ISNULL(@wbsno, '') = '' OR WBS_NO = ISNULL(@wbsno, '') )
+				AND WBS_NO = ISNULL(@wbsno, '') 
+		  UNION
+		  SELECT ASSET_NO
+			,SUB_ASSET_NO
+			,ASSET_CATEGORY
+			,CLASS_ID
+			,LOCATION_ID
+			,ASSET_DESC
+			,DIVISION_ID_HR
+			,COST_CENTER
+			,CREATED_DATE 
+			,WBS_NO
+			FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY LIKE '%AU%'
+					AND isnull(@DIVISION_PARAM, '') <> ''
+					OR (isnull(@DIVISION_PARAM, '') = '')))
+				/* FID.Ridwan: 20220712*/
+				AND LEFT(WBS_NO,8) = ISNULL(LEFT(@wbsno,8), '')
+	) tbl 
+	END
+	ELSE
+	BEGIN
+		
+		  SELECT  COUNT(*)	  
+			  FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY LIKE '%AU%'
+					AND isnull(@DIVISION_PARAM, '') <> ''
+					OR (isnull(@DIVISION_PARAM, '') = '')))
+				/* FID.Ridwan: 20220712*/
+				AND LEFT(WBS_NO,8) = ISNULL(LEFT(@wbsno,8), '')
+	END
+
 END
-ELSE
+ELSE --if Query Called from other controller (ex: for retrieve dropdown data)
 BEGIN
-	SELECT COUNT(1)
-	FROM [FAMS_DB].[FAMS_DB].[dbo].[GET_LIST_ASSET]
-		WHERE (((ASSET_NO LIKE '%' + ISNULL(@Param, '') + '%'
+		SELECT COUNT(*)
+			 FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE (((ASSET_NO LIKE '%' + ISNULL(@Param, '') + '%'
 					AND isnull(@Param, '') <> ''
 					OR (isnull(@Param, '') = '')))
 				AND ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
 					AND isnull(@DIVISION_PARAM, '') <> ''
 					OR (isnull(@DIVISION_PARAM, '') = '')))
-					)
 				/* FID.Ridwan: 20220712*/
 				AND (ISNULL(@wbsno, '') = '' OR WBS_NO = ISNULL(@wbsno, '') )
+			)
+
 END
+
+
--- GPS/GPS/SQL/PR/PRCreation/GetDataAssetNo.sql
+++ GPS/GPS/SQL/PR/PRCreation/GetDataAssetNo.sql
@@ -1,5 +1,8 @@
 ﻿IF(@Param = '') --IF Query called from screen Vendor Master
 BEGIN
+	DECLARE @@WbsParamSystem VARCHAR (10) = (SELECT TOP 1 ISNULL(SYSTEM_VALUE,'') FROM TB_M_SYSTEM WHERE FUNCTION_ID='ASSET' AND SYSTEM_CD='WBS_PARAM')
+	IF @wbsno LIKE CONCAT(@@WbsParamSystem,'%')
+	BEGIN
 	SELECT * FROM (
 		SELECT  ROW_NUMBER() OVER (ORDER BY [ASSET_NO] DESC) AS Number
 				  ,[ASSET_NO]
@@ -11,13 +14,55 @@ BEGIN
 				  ,[CREATED_BY]
 				  ,CONVERT(VARCHAR(10),[CREATED_DATE],23) AS [CREATED_DATE]
 				  
-			  FROM [FAMS_DB].[FAMS_DB].[dbo].[GET_LIST_ASSET]
+			  FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
 			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY NOT LIKE '%AU%'
 					AND isnull(@DIVISION_PARAM, '') <> ''
 					OR (isnull(@DIVISION_PARAM, '') = '')))
 				/* FID.Ridwan: 20220712*/
-				AND (ISNULL(@wbsno, '') = '' OR WBS_NO = ISNULL(@wbsno, '') )
+				AND WBS_NO = ISNULL(@wbsno, '') 
+		  UNION
+		  SELECT  ROW_NUMBER() OVER (ORDER BY [ASSET_NO] DESC) AS Number
+				  ,[ASSET_NO]
+				  ,[SUB_ASSET_NO]
+				  ,[ASSET_CATEGORY]
+				  ,[CLASS_ID]
+				  ,[LOCATION_ID]
+				  ,[ASSET_DESC]
+				  ,[CREATED_BY]
+				  ,CONVERT(VARCHAR(10),[CREATED_DATE],23) AS [CREATED_DATE]
+				  
+			  FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY LIKE '%AU%'
+					AND isnull(@DIVISION_PARAM, '') <> ''
+					OR (isnull(@DIVISION_PARAM, '') = '')))
+				/* FID.Ridwan: 20220712*/
+				AND LEFT(WBS_NO,8) = ISNULL(LEFT(@wbsno,8), '')
 	) tbl WHERE tbl.Number >= @Start AND tbl.Number <= @Length
+	END
+	ELSE
+	BEGIN
+		SELECT * FROM (
+		  SELECT  ROW_NUMBER() OVER (ORDER BY [ASSET_NO] DESC) AS Number
+				  ,[ASSET_NO]
+				  ,[SUB_ASSET_NO]
+				  ,[ASSET_CATEGORY]
+				  ,[CLASS_ID]
+				  ,[LOCATION_ID]
+				  ,[ASSET_DESC]
+				  ,[CREATED_BY]
+				  ,CONVERT(VARCHAR(10),[CREATED_DATE],23) AS [CREATED_DATE]
+				  
+			  FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
+			WHERE ((DIVISION_ID_HR LIKE '%' + ISNULL(@DIVISION_PARAM, '') + '%'
+					AND ASSET_CATEGORY LIKE '%AU%'
+					AND isnull(@DIVISION_PARAM, '') <> ''
+					OR (isnull(@DIVISION_PARAM, '') = '')))
+				/* FID.Ridwan: 20220712*/
+				AND LEFT(WBS_NO,8) = ISNULL(LEFT(@wbsno,8), '')
+	) tbl WHERE tbl.Number >= @Start AND tbl.Number <= @Length
+	END
 
 END
 ELSE --if Query Called from other controller (ex: for retrieve dropdown data)
@@ -32,7 +77,7 @@ BEGIN
 				  ,[ASSET_DESC]
 				  ,[CREATED_BY]
 				  ,CONVERT(VARCHAR(10),[CREATED_DATE],23) AS [CREATED_DATE]
-			  FROM [FAMS_DB].[FAMS_DB].[dbo].[GET_LIST_ASSET]
+			  FROM [FAMS_DB_SAPAHANA].[FAMS_DB_SAPHANA].[dbo].[VIEW_PAS_GET_LIST_ASSET]
 			WHERE (((ASSET_NO LIKE '%' + ISNULL(@Param, '') + '%'
 					AND isnull(@Param, '') <> ''
 					OR (isnull(@Param, '') = '')))
@@ -45,3 +90,4 @@ BEGIN
 	) tbl WHERE tbl.Number >= @Start AND tbl.Number <= @Length
 
 END
+
